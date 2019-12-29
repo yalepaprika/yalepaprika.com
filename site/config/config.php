2f8fffb833cec85a487dbbdce46035aa209c3925
Seth Thompson <s3th.thompson@gmail.com>
2019-12-28 18:52:41 -0700

    add markdown extra + smartypants

--- site/config/config.php
+++ site/config/config.php
@@ -3,6 +3,10 @@
 return [
     'panel' =>[
         'install' => true
-      ],
+    ],
+    'markdown' => [
+        'extra' => true
+    ],
+    "smartypants" => true,
     'url' => 'http://paprika.test/'
 ];
