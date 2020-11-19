69d59396add275ca561a5c082841cf58690ced99
Seth Thompson <s3th.thompson@gmail.com>
2020-11-19 02:05:09 -0500

    hide in-progress features

--- site/templates/fold.php
+++ site/templates/fold.php
@@ -8,7 +8,7 @@
       <?php snippet('fold/editors-statement') ?>
       <?php $details = snippet('fold/body-details', ['fold' => $page], true);
         snippet('page/body', ['page' => $page, 'details' => $details]) ?>
-      <?php snippet('fold/viewer', ['fold' => $page]) ?>
+      <!-- <?php snippet('fold/viewer', ['fold' => $page]) ?> -->
       <?php snippet('articles/list', ['articles' => $page->children()]) ?>
       <?php snippet('fold/details', ['fold' => $page]) ?>
       <?php snippet('fold/download', ['fold' => $page]) ?>
