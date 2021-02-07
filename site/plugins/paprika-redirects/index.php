9ad32604b0df5d2a9bf8216e1558e7253703bfa2
Seth Thompson <s3th.thompson@gmail.com>
2021-02-07 13:32:56 -0500

    make redirects permanent, add original wordpress slug redirects

--- site/plugins/paprika-redirects/index.php
+++ site/plugins/paprika-redirects/index.php
@@ -1,37 +1,48 @@
 <?php
 
+function findArticleBySlug($slug, $code = 301) {
+    $folds = site()->find('folds');
+    $articles = $folds->grandChildren();
+    foreach ($articles as $article) {
+        if ($article->slug() == $slug) {
+            go($article->url(), $code);
+        }
+    }
+}
+
 Kirby::plugin('yalepaprika/paprika-redirects', [
   'routes' =>  [
             [
                 'pattern' => 'articles/(:any)',
                 'action' => function ($any) {
-                    $folds = site()->find('folds');
-                    $articles = $folds->grandChildren();
-                    foreach ($articles as $article) {
-                        if ($article->slug() == $any) {
-                            go($article->url());
-                        }
-                    }
+                    findArticleBySlug($any);
                     $this->next();
                 }
             ],
             [
                 'pattern' => 'archive/folds',
                 'action' => function () {
-                    go(site()->find('folds')->url());
+                    go(site()->find('folds')->url(), 301);
                 }
             ],
             [
                 'pattern' => 'archive/articles',
                 'action' => function () {
-                    go('/articles');
+                    go('/articles', 301);
                 }
             ],
             [
                 'pattern' => 'archive/contributors',
                 'action' => function () {
-                    go(site()->find('contributors')->url());
+                    go(site()->find('contributors')->url(), 301);
                 }
-            ]
+            ],
+            [
+                'pattern' => '(:any)',
+                'action' => function ($any) {
+                    findArticleBySlug($any);
+                    $this->next();
+                }
+            ],
         ]
 ]);
