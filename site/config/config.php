bbcf4bae9bf06bc57d767eecbcb5296dace3e927
Seth Thompson <s3th.thompson@gmail.com>
2021-02-16 13:43:46 -0500

    add sitemap to robots.txt

--- site/config/config.php
+++ site/config/config.php
@@ -5,6 +5,7 @@ return [
         'extra' => true
     ],
     'smartypants' => true,
+    'bnomei.robots-txt.sitemap' => \Kirby\Http\URL::to('/sitemap.xml'),
     'omz13.xmlsitemap.disableImages' => true,
     'omz13.xmlsitemap.includeUnlistedWhenTemplateIs' => [
         'fold',
