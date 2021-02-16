3f94d430f666cae96ba304fbc6ef0ec294176018
Seth Thompson <s3th.thompson@gmail.com>
2021-02-16 12:31:00 -0500

    add sitemap

--- site/config/config.php
+++ site/config/config.php
@@ -4,5 +4,12 @@ return [
     'markdown' => [
         'extra' => true
     ],
-    'smartypants' => true
+    'smartypants' => true,
+    'omz13.xmlsitemap.disableImages' => true,
+    'omz13.xmlsitemap.includeUnlistedWhenTemplateIs' => [
+        'fold',
+        'folds',
+        'contributor',
+        'contributors'
+    ]
 ];
