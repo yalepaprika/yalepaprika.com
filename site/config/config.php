d6e76de26ea7ed0a371e133e9a2f2561f90b81b2
Seth Thompson <s3th.thompson@gmail.com>
2021-02-16 15:37:13 -0500

    fix namespace error in config

--- site/config/config.php
+++ site/config/config.php
@@ -1,11 +1,13 @@
 <?php
 
+use \Kirby\Http\URL;
+
 return [
     'markdown' => [
         'extra' => true
     ],
     'smartypants' => true,
-    'bnomei.robots-txt.sitemap' => \Kirby\Http\URL::to('/sitemap.xml'),
+    'bnomei.robots-txt.sitemap' => URL::to('/sitemap.xml'),
     'omz13.xmlsitemap.disableImages' => true,
     'omz13.xmlsitemap.includeUnlistedWhenTemplateIs' => [
         'fold',
