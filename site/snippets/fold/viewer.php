8f03dc3bf8abc7575bb49067bc10bb1b154356b6
Seth Thompson <s3th.thompson@gmail.com>
2021-10-25 10:11:26 -0400

    add title

--- site/snippets/fold/viewer.php
+++ site/snippets/fold/viewer.php
@@ -16,5 +16,9 @@
   }
 ?>
 <div id="fold-viewer" class="fold-viewer">
+  <div class="fold-viewer-container box-card stack">
+    <h6><em><?= $fold->title() ?></em> <span class="separator">·</span> Volume <?= $fold->volume() ?> <span class="separator">·</span> Issue <?= sprintf('%02d', $fold->number()->value()) ?></h6>
+    <h6>25in × 22.75in <span class="separator">·</span> Offset print on newsprint</h6>
+  </div>
   <canvas id="fold-viewer-canvas" <?= Html::attr($attrs) ?>></canvas>
 </div>
