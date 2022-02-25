9497d8c40cf41a412b65e97ba264945d1cc5db2c
Seth Thompson <s3th.thompson@gmail.com>
2022-02-25 16:48:43 -0500

    add fold credits to contributors page

--- site/controllers/contributor.php
+++ site/controllers/contributor.php
@@ -2,11 +2,52 @@
 
 return function ($site, $page) {
 
-    $articles = $site->find('folds')->grandChildren()->filter(function($fold) use ($page) {
-        return $fold->contributors()->toPages()->has($page);
+    $articles = $site->find('folds')->grandChildren()->filter(function($article) use ($page) {
+        return $article->contributors()->toPages()->has($page);
+    });
+
+    $folds = $site->find('folds')->children();
+    $folds_as_fold_editor = $folds->filter(function($fold) use ($page) {
+        return $fold->fold_editors()->toPages()->has($page);
+    });
+
+    $folds_as_graphic_designer = $folds->filter(function($fold) use ($page) {
+        return $fold->graphic_designers()->toPages()->has($page);
+    });
+
+    $folds_as_coordinating_editor = $folds->filter(function($fold) use ($page) {
+        return $fold->coordinating_editors()->toPages()->has($page);
+    });
+
+    $folds_as_publisher = $folds->filter(function($fold) use ($page) {
+        return $fold->publishers()->toPages()->has($page);
+    });
+
+    $folds_as_archivist = $folds->filter(function($fold) use ($page) {
+        return $fold->archivists()->toPages()->has($page);
+    });
+
+    $folds_as_producer = $folds->filter(function($fold) use ($page) {
+        return $fold->producers()->toPages()->has($page);
+    });
+
+    $folds_as_graphic_design_liaison = $folds->filter(function($fold) use ($page) {
+        return $fold->graphic_design_liaisons()->toPages()->has($page);
+    });
+
+    $folds_as_web_editor = $folds->filter(function($fold) use ($page) {
+        return $fold->web_editors()->toPages()->has($page);
     });
 
     return [
-        'articles' => $articles
+        'articles' => $articles,
+        'folds_as_fold_editor' => $folds_as_fold_editor,
+        'folds_as_graphic_designer' => $folds_as_graphic_designer,
+        'folds_as_coordinating_editor' => $folds_as_coordinating_editor,
+        'folds_as_publisher' => $folds_as_publisher,
+        'folds_as_archivist' => $folds_as_archivist,
+        'folds_as_producer' => $folds_as_producer,
+        'folds_as_graphic_design_liaison' => $folds_as_graphic_design_liaison,
+        'folds_as_web_editor' => $folds_as_web_editor
     ];
 };
--- site/templates/contributor.php
+++ site/templates/contributor.php
@@ -7,6 +7,14 @@
         <?php snippet('page/title', ['title' => $page->title()]) ?>
         <div class="box-block box-block-ruled stack">
           <?php snippet('utilities/detail-list/pages-y', ['title' => 'Articles', 'pages' => $articles]) ?>
+          <?php snippet('utilities/detail-list/pages-y', ['title' => 'Folds (Fold Editor)', 'pages' => $folds_as_fold_editor]) ?>
+          <?php snippet('utilities/detail-list/pages-y', ['title' => 'Folds (Graphic Designer)', 'pages' => $folds_as_graphic_designer]) ?>
+          <?php snippet('utilities/detail-list/pages-y', ['title' => 'Folds (Coordinating Editor)', 'pages' => $folds_as_coordinating_editor]) ?>
+          <?php snippet('utilities/detail-list/pages-y', ['title' => 'Folds (Publisher)', 'pages' => $folds_as_publisher]) ?>
+          <?php snippet('utilities/detail-list/pages-y', ['title' => 'Folds (Archivist)', 'pages' => $folds_as_archivist]) ?>
+          <?php snippet('utilities/detail-list/pages-y', ['title' => 'Folds (Producer)', 'pages' => $folds_as_producer]) ?>
+          <?php snippet('utilities/detail-list/pages-y', ['title' => 'Folds (Graphic Design Liaison)', 'pages' => $folds_as_graphic_design_liaison]) ?>
+          <?php snippet('utilities/detail-list/pages-y', ['title' => 'Folds (Web Editor)', 'pages' => $folds_as_web_editor]) ?>
         </div>
       </article>
     </main>
