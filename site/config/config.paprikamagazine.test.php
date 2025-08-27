4bce8e349cfb141e48fcc0be75c534df161fc1f9
Seth Thompson <s3th.thompson@gmail.com>
2025-08-27 08:39:31 -0400

    add support for paprikamagazine.com

---
+++ site/config/config.paprikamagazine.test.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'debug' => true,
+    "cache" => false,
+    'panel' => [
+        'install' => true
+    ],
+    'thumbs' => [
+        'driver' => 'im',
+        'bin' => '/opt/homebrew/bin/convert'
+    ],
+    'yalepaprika.renderer.bin' =>'/opt/homebrew/bin/convert'
+];
